b7918b92d869bdb91a285aed6817cbb747aa1de1
siobhanlgorman <siobhanlgorman@gmail.com>
2021-08-24 16:59:55 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,6 +36,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -69,3 +72,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
