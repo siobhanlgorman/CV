16cc5d81a34385c833d9ae2308c15c901c804795
siobhanlgorman <siobhanlgorman@gmail.com>
2021-08-25 12:41:44 +0000

    Add code to send email

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,20 @@
+// function sendMail(contactForm) {
+//     emailjs.send("service_95zi4rj", "template_t06u7zf", 
+//     {
+//         "from_name": contactForm.fullname.value,
+//         "from_email": contactForm.emailaddress.value,
+//         "project_request": contactForm.projectsummary.value
+//         },
+//         "user_H7tvrweGXTKl8Kp3i9chH"
+//         )
+//         .then(
+//             function(response) {
+//                 console.log("SUCCESS", response);
+
+//             },
+//             function(error) {
+//                 console.log("Failed", error)
+//             })
+//             return false;
+        
+// }
